af10b091651184529020c21ac2b9ccf6fe823d9a
eric-lab-star <cyon256@icloud.com>
2022-08-04 14:10:23 +0900

    java with vsc

---
+++ src/App.java
@@ -0,0 +1,14 @@
+
+/**
+ * App
+ * Date: 2022.08.04
+ * Author: eric-lab-star
+ * source:App.java
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("sdlkfjlkdlsfl");
+    }
+    
+}
